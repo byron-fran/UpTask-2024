5c15b02030b4655a2db8841603c77192a00833d7
Byron Francisco Chanax Itzep <1byron1999@gmail.com>
2024-06-20 17:07:18 -0700

    add model User

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import { connectDB } from './config/db'
 import ProjectRoutes from './routes/Project.routes'
 import cors from 'cors'
+import morgan from 'morgan'
 import { corsConfig } from './config/cors'
 
 dotenv.config()
@@ -11,6 +12,7 @@ const server = express();
 
 server.use(express.json())
 server.use(cors(corsConfig))
+server.use(morgan('dev'))
 server.use('/api/projects', ProjectRoutes)
 connectDB()
 export default server
