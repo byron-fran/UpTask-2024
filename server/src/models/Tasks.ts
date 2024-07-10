bd835a2a413b83de4aa90d2661b3021ec08e8349
Byron Francisco Chanax Itzep <1byron1999@gmail.com>
2024-07-10 15:19:25 -0700

    add new field in Task table

--- server/src/models/Tasks.ts
+++ server/src/models/Tasks.ts
@@ -1,44 +1,49 @@
 import { toString } from "express-validator/lib/utils";
-import mongoose, {Schema, Document, Types} from "mongoose";
+import mongoose, { Schema, Document, Types } from "mongoose";
 
 const taskStatus = {
-    PENDING : 'pending',
-    ON_HOLD : 'onHold',
-    IN_PROGRESS :  'inProgress',
-    UNDER_REVIEW : 'underReview',
-    COMPLETED : 'completed'
+    PENDING: 'pending',
+    ON_HOLD: 'onHold',
+    IN_PROGRESS: 'inProgress',
+    UNDER_REVIEW: 'underReview',
+    COMPLETED: 'completed'
 } as const
 
 export type TaskStatus = typeof taskStatus[keyof typeof taskStatus]
 
-export interface TasksInterface extends Document  {
-    name : string,
-    description : string,
-    project : Types.ObjectId,
-    status : TaskStatus
+export interface TasksInterface extends Document {
+    name: string,
+    description: string,
+    project: Types.ObjectId,
+    status: TaskStatus,
+    completedBy: Types.ObjectId
 }
 
 export const TaskSchema = new Schema({
-    name : {
-        type : String,
-        required : true,
-        trim : true
+    name: {
+        type: String,
+        required: true,
+        trim: true
     },
-    description : {
-        type : String,
-        required : true,
-        trim : true
+    description: {
+        type: String,
+        required: true,
+        trim: true
     },
-    project : {
-        type : Types.ObjectId,
-        ref : 'Project'
+    project: {
+        type: Types.ObjectId,
+        ref: 'Project'
     },
-    status : {
-        type  : String,
-        enum : Object.values(taskStatus),
-        default : taskStatus.PENDING
+    status: {
+        type: String,
+        enum: Object.values(taskStatus),
+        default: taskStatus.PENDING
+    },
+    completedBy: {
+        type: Types.ObjectId,
+        ref: 'User'
     }
-}, {timestamps : true})
+}, { timestamps: true })
 
 const Task = mongoose.model<TasksInterface>('Task', TaskSchema)
 export default Task
