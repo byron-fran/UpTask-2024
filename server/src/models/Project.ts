8dc65cfc89ced71a53ed07db0770b75149244a6b
Byron Francisco Chanax Itzep <1byron1999@gmail.com>
2024-07-13 13:52:22 -0700

    add middleware to delete many tasks and notes when delete a project

--- server/src/models/Project.ts
+++ server/src/models/Project.ts
@@ -1,53 +1,67 @@
-import mongoose, {Schema, Document, PopulatedDoc, Types} from "mongoose";
-import { TasksInterface } from "./Tasks";
+import mongoose, { Schema, Document, PopulatedDoc, Types } from "mongoose";
+import Task, { TasksInterface } from "./Tasks";
 import { UserInterface } from "./User";
+import Note from "./Note";
 
-export interface ProjectInterface extends Document  {
-    projectName : string,
-    clientName : string,
-    description : string,
-    tasks : PopulatedDoc<TasksInterface & Document>[],
-    manager : PopulatedDoc<UserInterface & Document>,
-    team : PopulatedDoc<UserInterface & Document>[]
+export interface ProjectInterface extends Document {
+    projectName: string,
+    clientName: string,
+    description: string,
+    tasks: PopulatedDoc<TasksInterface & Document>[],
+    manager: PopulatedDoc<UserInterface & Document>,
+    team: PopulatedDoc<UserInterface & Document>[]
 
 }
 
 const ProjectSchema: Schema = new Schema({
-    projectName : {
-        type : String,
-        required : true,
-        trim : true,
-        
+    projectName: {
+        type: String,
+        required: true,
+        trim: true,
+
     },
-    clientName : {
-        type : String,
-        required : true,
-        trim : true,
+    clientName: {
+        type: String,
+        required: true,
+        trim: true,
     },
-    description : {
-        type : String,
-        required : true,
-        trim : true,
+    description: {
+        type: String,
+        required: true,
+        trim: true,
     },
-    tasks : [
+    tasks: [
         {
-            type : Types.ObjectId,
-            ref : 'Task'
+            type: Types.ObjectId,
+            ref: 'Task'
         }
     ],
-    manager : {
-        type : Types.ObjectId,
-        ref : 'User'
+    manager: {
+        type: Types.ObjectId,
+        ref: 'User'
     },
 
-    team : [
+    team: [
         {
-            type : Types.ObjectId,
-            ref : 'User'
+            type: Types.ObjectId,
+            ref: 'User'
         }
     ],
+
+}, { timestamps: true });
+
+ProjectSchema.pre('deleteOne', { document: true }, async function () {
+    const projectId = this._id;
+    if (!projectId) return;
+
+    const tasks = await Task.find({ project: projectId });
+    for (const task of tasks) {
+        await Note.deleteMany({ task: task.id })
+    };
     
-}, {timestamps : true})
+    await Task.deleteMany({ project: projectId })
+
+})
 
 const Project = mongoose.model<ProjectInterface>('Project', ProjectSchema)
 export default Project
